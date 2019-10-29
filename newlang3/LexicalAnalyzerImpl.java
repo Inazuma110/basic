b7bbf4a3eb32a56f38ffabfca94a21039ea0e8f1
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-10-29 16:24:00 +0900

    lec03

--- newlang3/LexicalAnalyzerImpl.java
+++ newlang3/LexicalAnalyzerImpl.java
@@ -1,18 +1,47 @@
-package newlang3
+package newlang3;
 
 public class LexicalAnalyzerImpl implements LexicalAnalyzer{
+  private FileReader fr;
+  public LexicalAnalyzerImpl(String fname){
+    try{
+      this.fr = new FileReader(fname);
+    }catch(FileNotFoundException e){
+      e.printStackTrace();
+    }
+
+  }
 
   @Override
   public LexicalUnit get() throws Exception{
+    char c = (char)fr.read();
+    while(c == ' ' || c == '\t'){
+      c = (char)fr.read();
+    }
+
+    if(c >= '0' && c <= '9'){
+      return intGet(c);
+    }
+    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+      return stringGet(c);
+    }
+    if(c == '"'){
+      return literalGet();
+    }
+
+    return specialGet();
   }
 
   @Override
   public boolean except(LexicalType type) throws Exception{
-
+    return false;
   }
 
   @Override
   public void unget(LexicalUnit token) throws Exception{
 
+  }
+
+  @Override
+  public String toString(){
   }
 }
--- newlang3/Main.java
+++ newlang3/Main.java
@@ -4,6 +4,18 @@ import newlang3.LexicalAnalyzer;
 public class Main {
   public static void main(String args[]) {
     // LexicalAnalyzer analyzer = new LexicalAnalyzer("main.bas");
+    String fname = "Main.bas";
+    LexicalAnalyzer analyzer = new LexicalAnalyzerImpl(fname);
 
+    while(true){
+      try{
+        LexicalUnit unit = analyzer.get();
+      }catch(Exception e){
+        e.printStackTrace();
+        return;
+      }
+      System.out.println(unit);
+      if(analyzer.getType() == LexicalType.EOF) break;
+    }
   }
 }
